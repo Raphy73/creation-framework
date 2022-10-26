8430f72c0c344ce254094af78535eae2672c7e4a
Raphaël Beaudet <raphaelbeaudet@icloud.com>
2022-10-26 16:45:05 +0200

    WIP : repository

--- src/main/java/app/Repository/PartnerRepository.java
+++ src/main/java/app/Repository/PartnerRepository.java
@@ -1,2 +1,34 @@
-package app.Repository;public class PartnerRepository {
+package app.Repository;
+
+import app.Model.Partner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class PartnerRepository {
+    private static PartnerRepository instance;
+    public static List<Partner> allTheElements;
+
+    private PartnerRepository()
+    {
+        allTheElements = new ArrayList<>();
+    }
+
+    private static PartnerRepository getInstance()
+    {
+        if(instance == null) {
+            instance = new PartnerRepository();
+        }
+        return instance;
+    }
+
+    public static void add(Partner partner)
+    {
+        allTheElements.add(partner);
+    }
+
+    public static List<Partner> findAll()
+    {
+        return allTheElements;
+    }
 }
