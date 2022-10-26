26e49b69a4c89c67e25c087b4b0f5f2cc147b5e7
Raphaël Beaudet <raphaelbeaudet@icloud.com>
2022-10-26 15:44:42 +0200

    finished the creation of all the controllers and models

--- src/main/java/lajavel/Contributor.java
+++ src/main/java/lajavel/Contributor.java
@@ -1,2 +1,27 @@
-package lajavel;public class Contributor {
+package lajavel;
+
+public class Contributor {
+    public String firstName;
+
+    public String lastName;
+    public String urlImage;
+    public String job;
+    public String description;
+    public String urlTwitter;
+
+
+    public Contributor(String firstName, String lastName, String urlImage, String job, String description, String urlTwitter)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.urlImage = urlImage;
+        this.job = job;
+        this.description = description;
+        this.urlTwitter = urlTwitter;
+    }
+
+    public String getFirstnameAndLastname()
+    {
+        return firstName + " " + lastName;
+    }
 }
--- src/main/java/lajavel/Partner.java
+++ src/main/java/lajavel/Partner.java
@@ -1,2 +1,12 @@
-package lajavel;public class Partner {
+package lajavel;
+
+public class Partner {
+    public String nameImg;
+    public String website;
+
+    public Partner(String nameImg, String website)
+    {
+        this.nameImg = nameImg;
+        this.website = website;
+    }
 }
