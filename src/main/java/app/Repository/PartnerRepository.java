03fc42c4f8c95ff65115d8b061af11e5c017759c
Raphaël Beaudet <raphaelbeaudet@icloud.com>
2022-11-18 08:52:12 +0100

    creation of the partner repository

--- src/main/java/app/Repository/PartnerRepository.java
+++ src/main/java/app/Repository/PartnerRepository.java
@@ -2,16 +2,24 @@ package app.Repository;
 
 import app.Model.Partner;
 
+import javax.servlet.http.Part;
 import java.util.ArrayList;
 import java.util.List;
 
 public final class PartnerRepository {
     private static PartnerRepository instance;
-    public static List<Partner> allTheElements;
+    private final List<Partner> allTheElements;
 
     private PartnerRepository()
     {
-        allTheElements = new ArrayList<>();
+        Partner partner1 = new Partner("1-client", "http://www.boucherie-ayet-chambery.com");
+        Partner partner2 = new Partner("2-client", "https://www.skyrock.com/blog/");
+        Partner partner3 = new Partner("3-client", "https://www.alcatelmobile.com/fr/");
+        Partner partner4 = new Partner("4-client", "https://jl2022.fr");
+        Partner partner5 = new Partner("5-client", "https://www.facebook.com/people/Les-fromages-à-DUDUL/100031928752636/");
+        Partner partner6 = new Partner("6-client", "https://www.msn.com/fr-fr");
+
+        this.allTheElements = List.of(partner1, partner2, partner3, partner4, partner5, partner6);
     }
 
     private static PartnerRepository getInstance()
@@ -21,14 +29,13 @@ public final class PartnerRepository {
         }
         return instance;
     }
-
-    public static void add(Partner partner)
-    {
-        allTheElements.add(partner);
-    }
-
     public static List<Partner> findAll()
     {
-        return allTheElements;
+        return PartnerRepository.getInstance().allTheElements;
+    }
+
+    public static Partner findOne(int id)
+    {
+        return PartnerRepository.getInstance().allTheElements.get(id);
     }
 }
